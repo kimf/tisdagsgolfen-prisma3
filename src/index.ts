d352cc0ad70e96615bc5c78f612a30b5bca5db2b
Kim Fransman <kim.fransman@gmail.com>
2021-11-06 16:31:53 +0100

    Let's deploy to heroku

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { context } from './context';
 const app = async () => {
   const schema = await tq.buildSchema({ resolvers });
 
-  new ApolloServer({ schema, context: context }).listen({ port: 4000 }, () =>
+  new ApolloServer({ schema, context: context }).listen({ port: process.env.PORT || 4000 }, () =>
     console.log(`🚀 Server ready at: http://localhost:4000\n⭐️`),
   );
 };
